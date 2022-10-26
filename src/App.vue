517d99b2ce199943c5fb3cf943a5be43a1662bd1
Pyrta <pyrta90@gmail.com>
2022-10-26 09:19:29 +0400

    Initialize app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>It's work</h1>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+* {
+   margin: 0;
+   padding: 0;
+   box-sizing: border-box;
+}
+#app {
+  font-family: Ubuntu;
+}
+</style>
